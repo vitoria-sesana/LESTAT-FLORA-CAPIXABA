8e0df8fea096fa048425ecdc2df80338d9106774
vitoria-sesana <vitoriasesana11@gmail.com>
2025-10-05 16:39:45 -0300

    Update analises iniciais

--- scripts/a-descritiva/a-leitura.R
+++ scripts/a-descritiva/a-leitura.R
@@ -1,9 +1,15 @@
 library(dplyr)
+library(sf)
 
 # leitura -----------------------------------------------------------------
-
+# base flora
 caminho_flora <- "bases/2025-10-01_Estatísticas Básica.xlsx"
 
 base_flora <- 
   readxl::read_xlsx(caminho, sheet = "Flora")
 
+
+map <- sf::st_read("bases/QGis_Vetores-Matriciais/vetor/geofisic/Clima-Brasil_IBGE_2002.shp")
+class(map)
+head(map)
+plot(map)
